b2ae35f58bbc3ae398b76436872569f648a23270
rajurao <rajurao1107@gmail.com>
2025-06-13 18:22:32 +0530

    commit

--- src/homepage/Hero/Hero1.jsx
+++ src/homepage/Hero/Hero1.jsx
@@ -41,7 +41,7 @@ export default function Hero1() {
                   className="object-cover -scale-x-100 h-[35rem] max-lg:h-[25rem] pt-2"
                 />
     
-                <div className="absolute bottom-60 max-lg:bottom-40 -left-24 max-lg:-left-15 bg-white border border-blue-200 shadow-lg rounded-lg px-4 max-lg:px-1 py-4 max-lg:py-2 flex items-center gap-4">
+                <div className="absolute bottom-60 max-lg:bottom-40 -left-24 max-lg:-left-12 bg-white border border-blue-200 shadow-lg rounded-lg px-4 max-lg:px-1 py-4 max-lg:py-2 flex items-center gap-4">
                   <div className="text-5xl max-lg:text-2xl text-blue-500">
                     <IoCall />
                   </div>
@@ -57,8 +57,8 @@ export default function Hero1() {
                   </div>
                   <div>
                     <h1 className="font-semibold text-xl max-lg:text-base font-[Noto_Sans]">Opening Hours</h1>
-                    <p className="max-lg:text-sm">Mon to Sat 10 AM - 2 PM / 4 PM - 8 PM</p>
-                    <p className="max-lg:text-sm">Sunday 10 AM - 2 PM</p>
+                    <p className="max-lg:text-sm">Mon to Sat : 10 AM - 2 PM / 4 PM - 8 PM</p>
+                    <p className="max-lg:text-sm">Sunday : 10 AM - 2 PM</p>
                   </div>
                 </div>
               </div>
